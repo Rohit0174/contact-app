3cb5da9636bbfb0e61c2ed0cd2fd2e7c28cdaf94
Rohit Maheshwari <rohit.maheshwari@createbytes.com>
2022-08-21 21:43:57 +0530

    Rendered contact list

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -2,10 +2,21 @@ import React from "react";
 
 const ContactList = (props) =>{
     console.log("hello",props);
+    const renderContactList = props.contacts.map((contact)=>{
+        return (
+            <div className="item">
+                <div className="content">
+                    <div className="Header">{contact.name}</div>
+                    <div className="Email">{contact.email}</div>
+                </div>
+                <i className="trash alternate outline icon"/>
+            </div>
+        );
+    });
     return (
         <div className="ui called list">
-            Contact List
+            {renderContactList}
         </div>
-    )
+    );
 }
 export default ContactList;
